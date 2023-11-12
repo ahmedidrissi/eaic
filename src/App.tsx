8dd6db46e142b2d22700946c1b4cc910ade86264
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 18:58:29 +0100

    Add navbar and footer components, and comment out unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
-import Home from "./components/home/Home";
+import NavBar from "./components/shared/navbar/NavBar";
+import Footer from "./components/shared/footer/Footer";
+// import Home from "./components/home/Home";
+// import About from "./components/about/About";
+// import Contact from "./components/contact/Contact";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -10,7 +15,13 @@ function App() {
           color: "var(--main-text-color)",
         }}
       >
-        <Home />
+        <NavBar />
+        {/* <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        </Switch> */}
+        <Footer />
       </div>
     </>
   );
