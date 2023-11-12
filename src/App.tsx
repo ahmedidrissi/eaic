643149f3b03a9dfeb8d7b37564a1f862f8bc1181
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 19:08:12 +0100

    Refactor routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,23 @@
-import NavBar from "./components/shared/navbar/NavBar";
-import Footer from "./components/shared/footer/Footer";
-// import Home from "./components/home/Home";
-// import About from "./components/about/About";
-// import Contact from "./components/contact/Contact";
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-      <div
-        style={{
-          backgroundColor: "var(--main-bg-color)",
-          color: "var(--main-text-color)",
-        }}
-      >
-        <NavBar />
-        {/* <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />
-        </Switch> */}
-        <Footer />
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
