1e42f91e6cd0725878af53cfaf0d00d21209ed7e
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 18:11:03 +0100

    Add ENSIAS AI Club footer to website

--- src/components/shared/footer/Footer.tsx
+++ src/components/shared/footer/Footer.tsx
@@ -1,12 +1,13 @@
-import ENSIAS_AI_Club_text from "../../assets/ENSIAS_AI_Club_text.png";
-import ENSIAS_AI_Club_logo_white from "../../assets/ENSIAS_AI_Club_logo_white.png";
+import ENSIAS_AI_Club_text from "../../../assets/ENSIAS_AI_Club_text.png";
+import ENSIAS_AI_Club_logo_white from "../../../assets/ENSIAS_AI_Club_logo_white.png";
+import "./Footer.css";
 
 function Footer() {
   const year = 2023;
 
   return (
     <footer className="container-fluid">
-      <div className="footer-top row row-cols-1 row-cols-sm-2 row-cols-md-6 py-5 border-top bg-dark text-light">
+      <div className="footer-top row row-cols-1 row-cols-sm-2 row-cols-md-6 py-5 border-top border-dark text-light">
         <div className="col mb-3"></div>
 
         <div className="col mb-3 d-flex flex-column align-items-center align-items-lg-start">
@@ -28,7 +29,7 @@ function Footer() {
                 href="http://ensias.um5.ac.ma/"
                 className="nav-link p-0 text-light"
               >
-                Website
+                <i className="bi bi-chevron-right"></i> Website
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -36,7 +37,7 @@ function Footer() {
                 href="https://www.linkedin.com/company/ensias/"
                 className="nav-link p-0 text-light"
               >
-                LinkedIn
+                <i className="bi bi-chevron-right"></i> LinkedIn
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -44,7 +45,7 @@ function Footer() {
                 href="https://www.facebook.com/ensias.official"
                 className="nav-link p-0 text-light"
               >
-                Facebook
+                <i className="bi bi-chevron-right"></i> Facebook
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -52,7 +53,7 @@ function Footer() {
                 href="https://www.instagram.com/ensias.official/?hl=fr"
                 className="nav-link p-0 text-light"
               >
-                Instagram
+                <i className="bi bi-chevron-right"></i> Instagram
               </a>
             </li>
           </ul>
@@ -68,7 +69,7 @@ function Footer() {
                 href="https://www.linkedin.com/company/adei-ensias/"
                 className="nav-link p-0 text-light"
               >
-                LinkedIn
+                <i className="bi bi-chevron-right"></i> LinkedIn
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -76,7 +77,7 @@ function Footer() {
                 href="https://www.facebook.com/adei.ensias.3"
                 className="nav-link p-0 text-light"
               >
-                Facebook
+                <i className="bi bi-chevron-right"></i> Facebook
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -84,7 +85,7 @@ function Footer() {
                 href="https://www.instagram.com/adei.ensias/"
                 className="nav-link p-0 text-light"
               >
-                Instagram
+                <i className="bi bi-chevron-right"></i> Instagram
               </a>
             </li>
             <li className="nav-item mb-2">
@@ -92,7 +93,7 @@ function Footer() {
                 href="https://twitter.com/des_adei"
                 className="nav-link p-0 text-light"
               >
-                Twitter
+                <i className="bi bi-chevron-right"></i> Twitter
               </a>
             </li>
           </ul>
@@ -103,66 +104,43 @@ function Footer() {
             <li className="mb-2">
               <h5>Follow us</h5>
             </li>
-            <li className="nav-item mb-3 d-flex justify-content-center align-items-center">
+            <li className="nav-item mb-3 d-flex justify-content-center align-items-center gap-2">
               <a
                 href="https://www.instagram.com/ensias_ai_club/"
                 className="nav-link p-0 text-light"
               >
-                <img
-                  src="https://img.icons8.com/ios-filled/50/7c3bed/instagram-new--v1.png"
-                  width="50"
-                  height="40"
-                  loading="lazy"
-                />
+                <i className="bi bi-instagram" style={{ fontSize: "40px" }}></i>
               </a>
               <a
                 href="https://www.facebook.com/ensiasai"
                 className="nav-link p-0 text-light"
               >
-                <img
-                  src="https://img.icons8.com/ios-filled/50/7c3bed/facebook--v1.png"
-                  width="50"
-                  height="40"
-                  loading="lazy"
-                />
+                <i className="bi bi-facebook" style={{ fontSize: "40px" }}></i>
               </a>
               <a
                 href="https://www.linkedin.com/company/ensiasai/"
                 className="nav-link p-0 text-light"
               >
-                <img
-                  src="https://img.icons8.com/ios-filled/50/7c3bed/linkedin.png"
-                  width="50"
-                  height="40"
-                  loading="lazy"
-                />
+                <i className="bi bi-linkedin" style={{ fontSize: "40px" }}></i>
               </a>
               <a
                 href="https://github.com/ensias-ai-club/"
                 className="nav-link p-0 text-light"
               >
-                <img
-                  src="https://img.icons8.com/ios-filled/50/7c3bed/github.png"
-                  width="50"
-                  height="40"
-                  loading="lazy"
-                />
+                <i className="bi bi-github" style={{ fontSize: "40px" }}></i>
               </a>
             </li>
             <li className="mb-2">
               <h5>Contact Details</h5>
             </li>
             <li className="nav-item mb-2">
-              <a
-                href="mailto:ensias.ai.club@gmail.com"
-                className="text-decoration-none"
-              >
-                ensias.ai.club@gmail.com
+              <a href="mailto:ensias.ai.club@gmail.com">
+                <i className="bi bi-envelope-at"></i> ensias.ai.club@gmail.com
               </a>
             </li>
             <li className="nav-item mb-2">
-              <a href="tel:+212622406448" className="text-decoration-none">
-                +212 6 22 40 64 48
+              <a href="tel:+212622406448">
+                <i className="bi bi-telephone"></i> +212 6 22 40 64 48
               </a>
             </li>
           </ul>
@@ -171,26 +149,12 @@ function Footer() {
         <div className="col mb-3"></div>
       </div>
 
-      <div className="footer-bottom bg-dark text-light">
-        <div className="d-flex flex-column flex-sm-row justify-content-center py-4 border-top">
-          <p>&copy; {year} ENSIAS AI Club ❤ All rights reserved.</p>
-          <ul className="list-unstyled d-flex">
-            <li className="ms-3">
-              <a className="link-body-emphasis" href="#">
-                <svg className="bi" width="24" height="24"></svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-body-emphasis" href="#">
-                <svg className="bi" width="24" height="24"></svg>
-              </a>
-            </li>
-            <li className="ms-3">
-              <a className="link-body-emphasis" href="#">
-                <svg className="bi" width="24" height="24"></svg>
-              </a>
-            </li>
-          </ul>
+      <div className="footer-bottom text-light">
+        <div className="copyright d-flex justify-content-center align-items-center py-4 border-top border-dark">
+          <p>
+            &copy; {year} ENSIAS AI Club <i className="bi bi-heart-fill"></i>{" "}
+            All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
