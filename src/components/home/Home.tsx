16e2cc157901a7a2330b30e188b0aeaa4f2bcbfa
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 15:45:21 +0100

    Add container and content to Home component

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -5,7 +5,44 @@ function Home() {
   return (
     <>
       <NavBar />
-      <div>Home</div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h1>Home</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos. Quisquam
+              quia, voluptatum, voluptates, quibusdam quidem voluptate
+              repellendus voluptas voluptatibus natus autem quos.
+            </p>
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   )
