eeec127c3bb3d4370c4e09b0475ab270e3eadaf5
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-12 15:45:29 +0100

    Refactor code for improved readability and performance.

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -5,128 +5,194 @@ function Footer() {
   const year = 2023;
 
   return (
-    <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-6 py-5 my-5 border-top bg-dark text-light">
-      <div className="col mb-3"></div>
+    <footer className="">
+      <div className="footer-top row row-cols-1 row-cols-sm-2 row-cols-md-6 py-5 border-top bg-dark text-light">
+        <div className="col mb-3"></div>
 
-      <div className="col mb-3 d-flex flex-column align-items-center align-items-lg-start">
-        <img src={ENSIAS_AI_Club_logo_white} width="126" alt="ENSIAS AI Club" />
-        <img src={ENSIAS_AI_Club_text} width="126" alt="ENSIAS AI Club" />
+        <div className="col mb-3 d-flex flex-column align-items-center align-items-lg-start">
+          <img
+            src={ENSIAS_AI_Club_logo_white}
+            width="126"
+            alt="ENSIAS AI Club"
+          />
+          <img src={ENSIAS_AI_Club_text} width="126" alt="ENSIAS AI Club" />
+        </div>
+
+        <div className="col mb-3">
+          <ul className="nav flex-column align-items-center align-items-lg-start">
+            <li className="mb-2">
+              <h5>ENSIAS</h5>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="http://ensias.um5.ac.ma/"
+                className="nav-link p-0 text-light"
+              >
+                Website
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.linkedin.com/company/ensias/"
+                className="nav-link p-0 text-light"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.facebook.com/ensias.official"
+                className="nav-link p-0 text-light"
+              >
+                Facebook
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.instagram.com/ensias.official/?hl=fr"
+                className="nav-link p-0 text-light"
+              >
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col mb-3">
+          <ul className="nav flex-column align-items-center align-items-lg-start">
+            <li className="mb-2">
+              <h5>ADEI ENSIAS</h5>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.linkedin.com/company/adei-ensias/"
+                className="nav-link p-0 text-light"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.facebook.com/adei.ensias.3"
+                className="nav-link p-0 text-light"
+              >
+                Facebook
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://www.instagram.com/adei.ensias/"
+                className="nav-link p-0 text-light"
+              >
+                Instagram
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="https://twitter.com/des_adei"
+                className="nav-link p-0 text-light"
+              >
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col mb-3">
+          <ul className="nav flex-column align-items-center align-items-lg-start">
+            <li className="mb-2">
+              <h5>Follow us</h5>
+            </li>
+            <li className="nav-item mb-3 d-flex justify-content-center align-items-center">
+              <a
+                href="https://www.instagram.com/ensias_ai_club/"
+                className="nav-link p-0 text-light"
+              >
+                <img
+                  src="https://img.icons8.com/ios-filled/50/7c3bed/instagram-new--v1.png"
+                  width="50"
+                  height="40"
+                  loading="lazy"
+                />
+              </a>
+              <a
+                href="https://www.facebook.com/ensiasai"
+                className="nav-link p-0 text-light"
+              >
+                <img
+                  src="https://img.icons8.com/ios-filled/50/7c3bed/facebook--v1.png"
+                  width="50"
+                  height="40"
+                  loading="lazy"
+                />
+              </a>
+              <a
+                href="https://www.linkedin.com/company/ensiasai/"
+                className="nav-link p-0 text-light"
+              >
+                <img
+                  src="https://img.icons8.com/ios-filled/50/7c3bed/linkedin.png"
+                  width="50"
+                  height="40"
+                  loading="lazy"
+                />
+              </a>
+              <a
+                href="https://github.com/ensias-ai-club/"
+                className="nav-link p-0 text-light"
+              >
+                <img
+                  src="https://img.icons8.com/ios-filled/50/7c3bed/github.png"
+                  width="50"
+                  height="40"
+                  loading="lazy"
+                />
+              </a>
+            </li>
+            <li className="mb-2">
+              <h5>Contact Details</h5>
+            </li>
+            <li className="nav-item mb-2">
+              <a
+                href="mailto:ensias.ai.club@gmail.com"
+                className="text-decoration-none"
+              >
+                ensias.ai.club@gmail.com
+              </a>
+            </li>
+            <li className="nav-item mb-2">
+              <a href="tel:+212622406448" className="text-decoration-none">
+                +212 6 22 40 64 48
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col mb-3"></div>
       </div>
 
-      <div className="col mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column align-items-center align-items-lg-start">
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Home
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Features
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              About
-            </a>
-          </li>
-        </ul>
+      <div className="footer-bottom bg-dark text-light">
+        <div className="d-flex flex-column flex-sm-row justify-content-center py-4 border-top">
+          <p>&copy; {year} ENSIAS AI Club ❤ All rights reserved.</p>
+          <ul className="list-unstyled d-flex">
+            <li className="ms-3">
+              <a className="link-body-emphasis" href="#">
+                <svg className="bi" width="24" height="24"></svg>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="link-body-emphasis" href="#">
+                <svg className="bi" width="24" height="24"></svg>
+              </a>
+            </li>
+            <li className="ms-3">
+              <a className="link-body-emphasis" href="#">
+                <svg className="bi" width="24" height="24"></svg>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-
-      <div className="col mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column align-items-center align-items-lg-start">
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Home
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Features
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              About
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="col mb-3">
-        <h5>Section</h5>
-        <ul className="nav flex-column align-items-center align-items-lg-start">
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Home
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Features
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item mb-2">
-            <a href="#" className="nav-link p-0 text-light">
-              About
-            </a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="col mb-3"></div>
-      {/* <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
-        <p>&copy; {year} Company, Inc. All rights reserved.</p>
-        <ul className="list-unstyled d-flex">
-          <li className="ms-3">
-            <a className="link-body-emphasis" href="#">
-              <svg className="bi" width="24" height="24"></svg>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="link-body-emphasis" href="#">
-              <svg className="bi" width="24" height="24"></svg>
-            </a>
-          </li>
-          <li className="ms-3">
-            <a className="link-body-emphasis" href="#">
-              <svg className="bi" width="24" height="24"></svg>
-            </a>
-          </li>
-        </ul>
-      </div> */}
     </footer>
   );
 }
