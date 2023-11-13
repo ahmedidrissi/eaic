ccb861e32002229199a89f1025f0fd709159cb5e
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-13 02:42:27 +0100

    Add new routes for data, training, and mlops components.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Layout from "./components/Layout";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Contact from "./components/contact/Contact";
+import Data from "./components/data/Data";
+import Training from "./components/training/Training";
+import MLops from "./components/mlops/MLops";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -15,6 +18,9 @@ function App() {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="data" element={<Data />} />
+            <Route path="training" element={<Training />} />
+            <Route path="mlops" element={<MLops />} />
           </Route>
         </Routes>
       </Router>
