d5fcd0b23ab04778deff08fa3eaec0728e47b37f
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-13 22:38:41 +0100

    Add session type, speakers, academic year, and tags to Session component

--- src/components/shared/session/Session.tsx
+++ src/components/shared/session/Session.tsx
@@ -3,15 +3,32 @@ import './Session.css';
 function Session(session: any) {
   return (
     <div className="card text-center text-light mb-3">
+        <div className="card-header">
+            <h6>{session.type}</h6>
+        </div>
       <img
         src={session.image ? session.image : "https://via.placeholder.com/400"}
         className="card-img-top"
       />
       <div className="card-body">
         <h5 className="card-title">{session.title}</h5>
-        <p className="card-subtitle">{session.type}</p>
-        {/* <p className="card-text">{session.speakers}</p> */}
-        <p className="card-text">{session.tags}</p>
+        <div className="card-text">
+          {session.speakers.map((speaker: any, index: any) => {
+            return (
+              <div key={index}>
+                <h6>{speaker}</h6>
+              </div>
+            );
+          })}   
+        </div>
+      </div>
+      <p className="card-subtitle mb-3">{session.academicYear}</p>
+      <div className='card-footer'>
+        {session.tags.map((tag: any, index:any) => {
+            return (
+            <button key={index} className="btn btn-sm m-1">{tag}</button>
+            )
+        })}
       </div>
     </div>
   );
