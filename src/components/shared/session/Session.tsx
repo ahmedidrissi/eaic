9c22627af4cb85be8814894d2a2b65dfc59b05e1
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-13 04:41:43 +0100

    Add image and styling to Session component

--- src/components/shared/session/Session.tsx
+++ src/components/shared/session/Session.tsx
@@ -1,17 +1,20 @@
+import './Session.css';
 
 function Session(session: any) {
   return (
-    <div className="card">
+    <div className="card text-center text-light mb-3">
+      <img
+        src={session.image ? session.image : "https://via.placeholder.com/400"}
+        className="card-img-top"
+      />
       <div className="card-body">
-        <h5 className="card-title">{session.name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{session.cell}</h6>
-        <p className="card-text">{session.type}</p>
-        <img src={session.image} alt="" />
-        <p className="card-text">{session.speackers}</p>
+        <h5 className="card-title">{session.title}</h5>
+        <p className="card-subtitle">{session.type}</p>
+        {/* <p className="card-text">{session.speakers}</p> */}
         <p className="card-text">{session.tags}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default Session
+export default Session;
