71e04d0b19c411d17b2c3e738f8d02fb7a4ea027
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-18 23:44:45 +0100

    Add session schema to models

---
+++ back/models/schemas.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const sessionSchema = new Schema({
+    title: {type: String, required: true},
+    cell: {type: String, required: true},
+    type: {type: String, required: true},
+    image: {type: String, required: true},
+    file: {type: String, required: true},
+    speakers: {type: [String], required: true},
+    tags: {type: [String], required: true},
+    academicYear: {type: String, required: true},
+});
+
+const Sessions = mongoose.model("Sessions", sessionSchema, "sessions");
+
+module.exports = Sessions;
