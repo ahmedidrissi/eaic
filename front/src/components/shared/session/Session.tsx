61b4c6e27098f7be40f9ff4ee4108d1d2d02e66d
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-19 14:35:32 +0100

    update

--- front/src/components/shared/session/Session.tsx
+++ front/src/components/shared/session/Session.tsx
@@ -1,13 +1,14 @@
-import './Session.css';
+import "./Session.css";
 
 function Session(session: any) {
   return (
     <div className="card text-center text-light mb-3">
-        <div className="card-header">
-            <h6>{session.type}</h6>
-        </div>
+      <div className="card-header">
+        <h6 style={{ textTransform: "uppercase" }}>{session.type}</h6>
+      </div>
       <img
         src={session.image ? session.image : "https://via.placeholder.com/400"}
+        style={{ width: "100%", height: "25vh", objectFit: "cover" }}
         className="card-img-top"
       />
       <div className="card-body">
@@ -19,17 +20,28 @@ function Session(session: any) {
                 <h6>{speaker}</h6>
               </div>
             );
-          })}   
+          })}
         </div>
       </div>
       <p className="card-subtitle mb-3">{session.academicYear}</p>
-      <div className='card-footer'>
-        {session.tags.map((tag: any, index:any) => {
-            return (
-            <button key={index} className="btn btn-sm m-1">{tag}</button>
-            )
-        })}
+      <div className="card-footer">
+        <a
+          href={session.file}
+          className="btn btn-sm w-100"
+          download
+        >
+          Download
+        </a>
       </div>
+      {/* <div className="card-footer">
+        {session.tags.map((tag: any, index: any) => {
+          return (
+            <button key={index} className="btn btn-sm m-1">
+              {tag}
+            </button>
+          );
+        })}
+      </div> */}
     </div>
   );
 }
