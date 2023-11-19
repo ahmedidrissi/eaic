c8646e9a2ca4447ef05ed90b37c1377afa2886f2
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-19 01:04:42 +0100

    Add training session data fetching and display

--- front/src/components/training/Training.tsx
+++ front/src/components/training/Training.tsx
@@ -1,22 +1,53 @@
+import { useEffect, useState } from "react";
+import Session from "../shared/session/Session";
+import { constants } from "../../constants/constants";
+
 function Training() {
+  const apiUrl = constants.apiUrl;
+
+  const [trainingSessions, setTrainingSessions] = useState([]);
+
+  useEffect(() => {
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTrainingSessions(data);
+      });
+  }, []);
+
   return (
     <>
-      <section className="section training" aria-label="training" id="training">
+      <section className="section data" aria-label="data" id="data">
         <div className="container">
-          <div className="title-wrapper">
+          <div className="title-wrapper mb-4">
             <h2
               className="headline-md section-title text-center"
               id="collection-label"
             >
-              Training session
+              Training Sessions
             </h2>
           </div>
-          <p style={{ textAlign: "justify", fontSize: "15pt" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vel
-            numquam quo reiciendis? Vel expedita quam doloremque accusantium
-            omnis alias tempora, eligendi porro quibusdam delectus sint facilis
-            ullam iste labore.
-          </p>
+          {/* check if dataSessions is empty */}
+          {trainingSessions.length > 0 ? (
+            <div className="row">
+            {trainingSessions.map((session: any) => (
+              <div className="col-lg-3 col-md-4 col-sm-6" key={session._id}>
+                <Session {...session} />
+              </div>
+            ))}
+          </div>
+          ) : (
+            <div className="row">
+              <div className="col-12">
+                <p className="text-center text-light">No Training Sessions Available</p>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </>
