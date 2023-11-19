a32d9463847eb086ec48dae07e2991f6269b4517
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-19 03:18:00 +0100

    Update API URL in constants.ts

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,4 +1,5 @@
 export const constants = {
     basePath: "/",
-    apiUrl: "https://eaic-react-app-fc1y.vercel.app/sessions"
+    apiUrl: "https://eaic-react-app-fc1y.vercel.app/api/v1",
+    // apiUrl: "http://localhost:5000/api/v1",
 };
