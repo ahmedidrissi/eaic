70da736aef9efc3e0e1441a3d6f656d8387b6e90
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-19 01:04:36 +0100

    Add MLOps session data fetching and rendering

--- front/src/components/mlops/MLOps.tsx
+++ front/src/components/mlops/MLOps.tsx
@@ -1,22 +1,53 @@
+import { useEffect, useState } from "react";
+import Session from "../shared/session/Session";
+import { constants } from "../../constants/constants";
+
 function MLOps() {
+  const apiUrl = constants.apiUrl;
+
+  const [mlopsSessions, setMlopsSessions] = useState([]);
+
+  useEffect(() => {
+    fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setMlopsSessions(data);
+      });
+  }, []);
+
   return (
     <>
-      <section className="section mlops" aria-label="mlops" id="mlops">
+      <section className="section data" aria-label="data" id="data">
         <div className="container">
-          <div className="title-wrapper">
+          <div className="title-wrapper mb-4">
             <h2
               className="headline-md section-title text-center"
               id="collection-label"
             >
-              MLOps sessions
+              MLOps Sessions
             </h2>
           </div>
-          <p style={{ textAlign: "justify", fontSize: "15pt" }}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni vel
-            numquam quo reiciendis? Vel expedita quam doloremque accusantium
-            omnis alias tempora, eligendi porro quibusdam delectus sint facilis
-            ullam iste labore.
-          </p>
+          {/* check if dataSessions is empty */}
+          {mlopsSessions.length > 0 ? (
+            <div className="row">
+            {mlopsSessions.map((session: any) => (
+              <div className="col-lg-3 col-md-4 col-sm-6" key={session._id}>
+                <Session {...session} />
+              </div>
+            ))}
+          </div>
+          ) : (
+            <div className="row">
+              <div className="col-12">
+                <p className="text-center text-light">No MLOps Sessions Available</p>
+              </div>
+            </div>
+          )}
         </div>
       </section>
     </>
