10f4310dc620dfa24d27777324bd98a6e96c89b7
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-20 22:37:32 +0100

    update

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,5 +1,4 @@
 export const constants = {
     basePath: "/",
-    apiUrl: "https://eaic-react-app-fc1y.vercel.app/api/v1",
-    // apiUrl: "http://localhost:5000/api/v1",
+    apiUrl: "http://localhost:5000/api/v1",
 };
