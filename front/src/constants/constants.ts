df8a13b3bc15ea8eaea5f5279656a7e6467cc732
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-20 22:49:17 +0100

    Update API URL in constants.ts

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
     basePath: "/",
-    apiUrl: "https://eaic-nltd-ahmed-idrissis-projects.vercel.app/api/v1",
+    apiUrl: "https://eaic-nltd.vercel.app/api/v1",
     // apiUrl: "http://localhost:5000/api/v1",
 };
