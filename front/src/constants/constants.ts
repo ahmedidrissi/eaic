cf29e54c133fa318a4a2056bb7e69b2a7b173908
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-21 02:19:48 +0100

    update apiUrl

--- front/src/constants/constants.ts
+++ front/src/constants/constants.ts
@@ -1,5 +1,5 @@
 export const constants = {
     basePath: "/",
-    apiUrl: "https://eaic-nltd.vercel.app/api/v1",
+    apiUrl: "https://eaic-vxma.vercel.app/api/v1",
     // apiUrl: "http://localhost:5000/api/v1",
 };
