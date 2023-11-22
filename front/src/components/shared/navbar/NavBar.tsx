2d886d9c97ee17355cab6076a26b4133b27a350d
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-22 10:30:24 +0100

    add close button

--- front/src/components/shared/navbar/NavBar.tsx
+++ front/src/components/shared/navbar/NavBar.tsx
@@ -11,84 +11,126 @@ function NavBar() {
   const currentPath = location.pathname;
 
   return (
-    <div className="navbar fixed-top navbar-expand-lg navbar-dark px-3">
-      <div className="container-fluid">
-        <div className="navbar-brand ms-lg-5">
-          <Link
-            to={basePath}
-            style={{ textDecoration: "none" }}
+    <>
+      <a
+        className="close-navbar-toggler collapsed"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      ></a>
+      <nav className="navbar fixed-top navbar-expand-lg navbar-dark px-3">
+        <div className="container-fluid">
+          <div className="navbar-brand ms-lg-5">
+            <Link to={basePath} style={{ textDecoration: "none" }}>
+              <img
+                src={ENSIAS_AI_Club_logo_white_text}
+                alt="ENSIAS AI Club"
+                width="125"
+                className="d-inline-block align-text-center"
+              />
+            </Link>
+            <a className="x-logo">
+              <img
+                src={x}
+                alt=""
+                width="30"
+                className="d-inline-block align-text-center me-3"
+              />
+            </a>
+            <a
+              href="https://www.datacamp.com/donates"
+              target="_blank"
+              className="dcd-logo"
+            >
+              <img
+                src={DCD_logo_inverted}
+                alt="DataCamp Donates"
+                width="120"
+                className="d-inline-block align-text-center"
+              />
+            </a>
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
           >
-            <img
-              src={ENSIAS_AI_Club_logo_white_text}
-              alt="ENSIAS AI Club"
-              width="125"
-              className="d-inline-block align-text-center"
-            />
-          </Link>
-          <a className="x-logo">
-            <img
-              src={x}
-              alt=""
-              width="30"
-              className="d-inline-block align-text-center me-3"
-            />
-          </a>
-          <a href="https://www.datacamp.com/donates" target="_blank" className="dcd-logo">
-            <img
-              src={DCD_logo_inverted}
-              alt="DataCamp Donates"
-              width="120"
-              className="d-inline-block align-text-center"
-            />
-          </a>
+            <span className="navbar-toggler-icon-close"><i className="bi bi-x-lg"></i></span>
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/about" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath + "about"}
+                >
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/data" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath + "data"}
+                >
+                  Data
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/training" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath + "training"}
+                >
+                  Training
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/mlops" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath + "mlops"}
+                >
+                  MLOps
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className={
+                    currentPath == "/contact" ? "nav-link active" : "nav-link"
+                  }
+                  to={basePath + "contact"}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto me-5 mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className={currentPath == "/" ? "nav-link active" : "nav-link"} to={basePath}>
-                Home
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className={currentPath == "/about" ? "nav-link active" : "nav-link"} to={basePath + "about"}>
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className={currentPath == "/data" ? "nav-link active" : "nav-link"} to={basePath + "data"}>
-                Data
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className={currentPath == "/training" ? "nav-link active" : "nav-link"} to={basePath + "training"}>
-                Training
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className={currentPath == "/mlops" ? "nav-link active" : "nav-link"} to={basePath + "mlops"}>
-                MLOps
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className={currentPath == "/contact" ? "nav-link active" : "nav-link"} to={basePath + "contact"}>
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+      </nav>
+    </>
   );
 }
 
