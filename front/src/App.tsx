aae3bd138e5b98c09dcfaf64bbe8c67421a55d04
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-26 21:48:20 +0100

    Add Projects component to App.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,7 @@ import Contact from "./components/contact/Contact";
 import Data from "./components/sessions/data/Data";
 import Training from "./components/sessions/training/Training";
 import MLOps from "./components/sessions/mlops/MLOps";
+import Projects from "./components/projects/Projects";
 import NotFound from "./components/notfound/NotFound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ScrollToTop from "./components/shared/scrolltotop/ScrollToTop";
@@ -24,6 +25,7 @@ function App() {
               <Route path="/data" element={<Data />} />
               <Route path="/training" element={<Training />} />
               <Route path="/mlops" element={<MLOps />} />
+              <Route path="/projects" element={<Projects />} />
               <Route path="*" element={<NotFound />} />
             </Route>
           </Routes>
