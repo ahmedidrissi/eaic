6e1003c04c323d8e1c8c544195a1fde8617d9023
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-26 22:04:07 +0100

    Comment out code

--- front/src/components/projects/Projects.tsx
+++ front/src/components/projects/Projects.tsx
@@ -3,90 +3,90 @@ import "./Projects.css";
 // import "slick-carousel/slick/slick.css";
 // import "slick-carousel/slick/slick-theme.css";
 
-const projects = [
-  {
-    name: "Project 1",
-    description: "This is the first project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 2",
-    description: "This is the second project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 3",
-    description: "This is the third project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 4",
-    description: "This is the fourth project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 5",
-    description: "This is the fifth project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 6",
-    description: "This is the sixth project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 7",
-    description: "This is the seventh project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-  {
-    name: "Project 8",
-    description: "This is the eighth project",
-    github: "https://github.com",
-    technologies: ["Python", "React"],
-  },
-];
+// const projects = [
+//   {
+//     name: "Project 1",
+//     description: "This is the first project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 2",
+//     description: "This is the second project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 3",
+//     description: "This is the third project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 4",
+//     description: "This is the fourth project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 5",
+//     description: "This is the fifth project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 6",
+//     description: "This is the sixth project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 7",
+//     description: "This is the seventh project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+//   {
+//     name: "Project 8",
+//     description: "This is the eighth project",
+//     github: "https://github.com",
+//     technologies: ["Python", "React"],
+//   },
+// ];
 
 function Projects() {
-  var settings = {
-    dots: true,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  // var settings = {
+  //   dots: true,
+  //   infinite: false,
+  //   speed: 500,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 4,
+  //   initialSlide: 0,
+  //   responsive: [
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 3,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 600,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 2,
+  //         initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 480,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
   return (
     <>
       <section
