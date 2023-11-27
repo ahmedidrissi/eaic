6d8df06d6841389fdcf1dba1d3cee86fa6adc973
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-27 14:54:02 +0100

    fix shimmer cards height

--- front/src/components/sessions/session/ShimmerSession.tsx
+++ front/src/components/sessions/session/ShimmerSession.tsx
@@ -3,16 +3,16 @@ import "./Session.css";
 function ShimmerSession() {
   return (
     <div className="session card text-center text-light mb-3 shimmerBG">
-      <img
-        style={{ width: "100%", height: "25vh", objectFit: "cover", visibility: "hidden" }}
-        className="card-img-top"
-      />
-      <div className="card-body">
-        <h5 className="card-title"></h5>
+      <img style={{ visibility: "hidden" }} className="card-img-top" />
+      <div className="card-body" style={{ visibility: "hidden" }}>
+        <h5 className="card-title">Loading ...</h5>
         <div className="card-text">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Exercitationem laboriosam non impedit dicta nobis libero minus ipsa
+          esse nesciunt, rerum animi saepe ipsam sequi.
         </div>
       </div>
-      <p className="card-subtitle mb-3"></p>
+      <p className="card-subtitle mb-3" style={{ visibility: "hidden" }}>Loading ...</p>
     </div>
   );
 }
