fd7f1e7c35ebe0f4adaf4bde0eb2a3c010843f3d
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-11-29 03:00:57 +0100

    optimize code and add illustrations

--- front/src/components/cells/Cells.tsx
+++ front/src/components/cells/Cells.tsx
@@ -1,4 +1,70 @@
+import visual_data from "../../assets/illustrations/visual_data.png";
+import chat_bot from "../../assets/illustrations/chat_bot.png";
+import server_cluster from "../../assets/illustrations/server_cluster.png";
+import business_deal from "../../assets/illustrations/business_deal.png";
+import conference from "../../assets/illustrations/conference.png";
+import social_media from "../../assets/illustrations/social_media.png";
+import designer from "../../assets/illustrations/designer.png";
 import "./Cells.css";
+
+const cells = [
+  {
+    id: 1,
+    name: "Data",
+    icon: "bi bi-bar-chart-line",
+    description:
+      "Our aim is to make you assimilate the basics of data science, data analytics through training projects and courses.",
+    image: visual_data,
+  },
+  {
+    id: 2,
+    name: "Training",
+    icon: "bi bi-laptop",
+    description:
+      "Our goal is to enhance your skills and knowledge in the field of artificial intelligence with a 'Learning-by-Doing' approach.",
+    image: chat_bot,
+  },
+  {
+    id: 3,
+    name: "MLOps",
+    icon: "bi bi-infinity",
+    description:
+      "Our aim is to equip you with the essential tools and techniques to ensure seamless integration of machine learning models into real-world applications.",
+    image: server_cluster,
+  },
+  {
+    id: 4,
+    name: "Sponsoring",
+    icon: "bi bi-suit-heart",
+    description:
+      "We take care of contacting the companies to guarantee the necessary resources to maintain the success of the events and activities.",
+    image: business_deal,
+  },
+  {
+    id: 5,
+    name: "Events",
+    icon: "bi bi-calendar-event",
+    description:
+      "Your creative idea can be realized in the events cell which oversees the realization of club events (hackathons, competitions ...) in their entirety.",
+    image: conference,
+  },
+  {
+    id: 6,
+    name: "Media",
+    icon: "bi bi-camera",
+    description:
+      "Our mission is to manage the club's social networks. This mainly involves covering the various club events, ensuring that each event gets the media coverage it deserves.",
+    image: social_media,
+  },
+  {
+    id: 7,
+    name: "Design",
+    icon: "bi bi-palette",
+    description:
+      "Takes care of the creation of all the posters and videos that you see in our social networks, so it will allow you to express your hidden artistic sense.",
+    image: designer,
+  },
+];
 
 function Cells() {
   return (
@@ -13,203 +79,43 @@ function Cells() {
               role="tablist"
               aria-orientation="vertical"
             >
-              <button
-                className="nav-link active"
-                id="v-pills-data-tab"
+              {cells.map((cell, index) => (
+                <button
+                key={cell.id}
+                className={`nav-link ${index === 0 ? "active" : ""}`}
+                id={`v-pills-${cell.name.toLowerCase()}-tab`}
                 data-bs-toggle="pill"
-                data-bs-target="#v-pills-data"
+                data-bs-target={`#v-pills-${cell.name.toLowerCase()}`}
                 type="button"
                 role="tab"
-                aria-controls="v-pills-data"
-                aria-selected="true"
+                aria-controls={`v-pills-${cell.name.toLowerCase()}`}
+                aria-selected={index === 0 ? "true" : "false"}
               >
-                <i className="bi bi-bar-chart-line"></i> Data
+                <i className={cell.icon}></i> {cell.name}
               </button>
-              <button
-                className="nav-link"
-                id="v-pills-training-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-training"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-training"
-                aria-selected="false"
-              >
-                <i className="bi bi-laptop"></i> Training
-              </button>
-              <button
-                className="nav-link"
-                id="v-pills-mlops-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-mlops"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-mlops"
-                aria-selected="false"
-              >
-                <i className="bi bi-infinity"></i> MLOps
-              </button>
-              <button
-                className="nav-link"
-                id="v-pills-sponsoring-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-sponsoring"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-sponsoring"
-                aria-selected="false"
-              >
-                <i className="bi bi-suit-heart"></i> Sponsoring
-              </button>
-              <button
-                className="nav-link"
-                id="v-pills-events-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-events"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-events"
-                aria-selected="false"
-              >
-                <i className="bi bi-calendar-event"></i> Events
-              </button>
-              <button
-                className="nav-link"
-                id="v-pills-media-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-media"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-media"
-                aria-selected="false"
-              >
-                <i className="bi bi-camera"></i> Media
-              </button>
-              <button
-                className="nav-link"
-                id="v-pills-design-tab"
-                data-bs-toggle="pill"
-                data-bs-target="#v-pills-design"
-                type="button"
-                role="tab"
-                aria-controls="v-pills-design"
-                aria-selected="false"
-              >
-                <i className="bi bi-palette"></i> Design
-              </button>
+              ))}
             </div>
             <div className="tab-content p-3" id="v-pills-tabContent">
-              <div
-                className="tab-pane fade show active"
-                id="v-pills-data"
+              {cells.map((cell, index) =>
+                <div
+                key={cell.id}
+                className={`tab-pane fade ${index === 0 ? "show active" : ""}`}
+                id={`v-pills-${cell.name.toLowerCase()}`}
                 role="tabpanel"
-                aria-labelledby="v-pills-data-tab"
+                aria-labelledby={`v-pills-${cell.name.toLowerCase()}-tab`}
               >
                 <div className="bg-icon">
-                  <i className="bi bi-bar-chart-line"></i>
+                  <i className={cell.icon}></i>
                 </div>
-                <h3>Data Cell</h3>
-                <p>
-                  Our aim is to make you assimilate the basics of data science,
-                  data analytics through training projects and courses.
-                </p>
+                <h3>{cell.name} Cell</h3>
+                <p>{cell.description}</p>
+                <img
+                  className="img-fluid rounded"
+                  src={cell.image}
+                  alt=""
+                />
               </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-training"
-                role="tabpanel"
-                aria-labelledby="v-pills-training-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-laptop"></i>
-                </div>
-                <h3>Training Cell</h3>
-                <p>
-                  Our goal is to enhance your skills and knowledge in the field
-                  of artificial intelligence with a "Learning-by-Doing"
-                  approach.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-mlops"
-                role="tabpanel"
-                aria-labelledby="v-pills-mlops-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-infinity"></i>
-                </div>
-                <h3>MLOps Cell</h3>
-                <p>
-                  Our aim is to equip you with the essential tools and
-                  techniques to ensure seamless integration of machine learning
-                  models into real-world applications.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-sponsoring"
-                role="tabpanel"
-                aria-labelledby="v-pills-sponsoring-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-suit-heart"></i>
-                </div>
-                <h3>Sponsoring Cell</h3>
-                <p>
-                  We take care of contacting the companies to guarantee the
-                  necessary resources to maintain the success of the events and
-                  activities.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-events"
-                role="tabpanel"
-                aria-labelledby="v-pills-events-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-calendar-event"></i>
-                </div>
-                <h3>Events Cell</h3>
-                <p>
-                  Your creative idea can be realized in the events cell which
-                  oversees the realization of club events (hackathons,
-                  competitions ...) in their entirety.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-media"
-                role="tabpanel"
-                aria-labelledby="v-pills-media-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-camera"></i>
-                </div>
-                <h3>Media Cell</h3>
-                <p>
-                  Our mission is to manage the club's social networks. This
-                  mainly involves covering the various club events, ensuring
-                  that each event gets the media coverage it deserves.
-                </p>
-              </div>
-              <div
-                className="tab-pane fade"
-                id="v-pills-design"
-                role="tabpanel"
-                aria-labelledby="v-pills-design-tab"
-              >
-                <div className="bg-icon">
-                  <i className="bi bi-palette"></i>
-                </div>
-                <h3>Design Cell</h3>
-                <p>
-                  Takes care of the creation of all the posters and videos that
-                  you see in our social networks, so it will allow you to
-                  express your hidden artistic sense.
-                </p>
-              </div>
+              )}
             </div>
           </div>
         </div>
