1be9a226d16142ac1b2b5e144fbc78b62dc2ff36
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-01 23:49:03 +0100

    Refactor Session component layout and add speaker names

--- front/src/components/sessions/session/Session.tsx
+++ front/src/components/sessions/session/Session.tsx
@@ -3,32 +3,53 @@ import "./Session.css";
 function Session(session: any) {
   return (
     <div className="session card text-center text-light mb-3">
-      <div className="card-header">
-        <h6 style={{ textTransform: "uppercase" }}>{session.type}</h6>
-      </div>
       <img
         src={session.image ? session.image : "https://via.placeholder.com/400"}
         className="card-img-top"
         loading="lazy"
       />
       <div className="card-body">
+        <div className="type mb-2 mt-1">
+          <span className="badge py-2">{session.type}</span>
+        </div>
         <h5 className="card-title">{session.title}</h5>
         <div className="card-text">
+          By
           {session.speakers.map((speaker: any, index: any) => {
             return (
               <div key={index}>
-                <h6>{speaker}</h6>
+                {speaker}
               </div>
             );
           })}
         </div>
       </div>
-      <p className="card-subtitle mb-3">{session.academicYear}</p>
+      <small className="card-subtitle mb-3">{session.academicYear}</small>
       <div className="card-footer">
         <a href={session.file} className="btn btn-sm w-100" download>
           Download
         </a>
       </div>
+      {/* 
+      <img
+        src={session.image ? session.image : "https://via.placeholder.com/400"}
+        className="card-img-top"
+        loading="lazy"
+      />
+      <div className="card-body">
+        <div className="type mb-2">
+          <span className="badge py-2">{session.type}</span>
+        </div>
+        <h5 className="card-title">{session.title}</h5>
+      </div>
+      <p className="card-subtitle mb-3">{session.academicYear}</p>
+      <div className="card-footer d-flex justify-content-between align-items-center">
+        <small>By {session.speakers.join(", ")}</small>
+        <a href={session.file} className="btn" download>
+          <i className="bi bi-download"></i>
+        </a>
+      </div>
+      */}
       {/* <div className="card-footer">
         {session.tags.map((tag: any, index: any) => {
           return (
