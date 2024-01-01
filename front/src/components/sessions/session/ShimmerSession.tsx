ca15f0ea095af8b72ec94deba97fbad923820abf
ahmedidrissi <idrissiahmed2002@gmail.com>
2024-01-01 23:45:08 +0100

    add bloc

--- front/src/components/sessions/session/ShimmerSession.tsx
+++ front/src/components/sessions/session/ShimmerSession.tsx
@@ -5,6 +5,9 @@ function ShimmerSession() {
     <div className="session card text-center text-light mb-3 shimmerBG">
       <img style={{ visibility: "hidden" }} className="card-img-top" />
       <div className="card-body" style={{ visibility: "hidden" }}>
+        <div className="type mb-2 mt-1">
+          <span className="badge py-2">Type</span>
+        </div>
         <h5 className="card-title">Loading ...</h5>
         <div className="card-text">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit.
@@ -12,7 +15,9 @@ function ShimmerSession() {
           esse nesciunt, rerum animi saepe ipsam sequi.
         </div>
       </div>
-      <p className="card-subtitle mb-3" style={{ visibility: "hidden" }}>Loading ...</p>
+      <p className="card-subtitle mb-3" style={{ visibility: "hidden" }}>
+        Loading ...
+      </p>
     </div>
   );
 }
