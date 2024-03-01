2c9f999a637bc6ba652e6322fab13dcf15d2ef21
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-03-01 11:15:39 +0100

    add Cells

--- front/src/components/about/About.tsx
+++ front/src/components/about/About.tsx
@@ -1,12 +1,6 @@
 import "./About.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
-import img1 from "../../assets/ai_day_1.jfif";
-import img2 from "../../assets/ai_day_2.jfif";
-import img3 from "../../assets/ai_day_3.jfif";
-import img4 from "../../assets/data_competition_2024.png";
-
-const imgList = [img1, img2, img3, img4];
+import Cells from "../cells/Cells";
+import Events from "../events/Events";
 
 function About() {
   return (
@@ -30,31 +24,9 @@ function About() {
             eager to learn and share their insights.
           </p>
 
-          <h2 className="text-center">Our Events</h2>
-          <div className="container p-3 d-flex justify-content-center">
-            <div
-              className="carousel py-3"
-              id="events"
-              style={{ maxWidth: "600px" }}
-            >
-              <Carousel
-                showThumbs={false}
-                showArrows={false}
-                infiniteLoop={true}
-                autoPlay={true}
-                stopOnHover={true}
-                showStatus={false}
-                swipeable={true}
-                width={"100%"}
-              >
-                {imgList.map((img, index) => (
-                  <div key={index}>
-                    <img src={img} />
-                  </div>
-                ))}
-              </Carousel>
-            </div>
-          </div>
+          <Cells />
+
+          <Events />
           <h2 className="text-center">Q&A</h2>
           <div className="container p-3 d-flex justify-content-center">
             <div className="accordion" id="qna">
